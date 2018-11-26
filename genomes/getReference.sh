1175f66baf6dc6c3c8ef9685c2ec519abd10b8cb
Christian Wünsch <Christian.Wuensch@ukmuenster.de>
2018-11-26 14:01:37 +0100

    Add hg19.crr to genomes

--- genomes/getReference.sh
+++ genomes/getReference.sh
@@ -1,1 +1,2 @@
 wget -O- https://amlvaran.uni-muenster.de/Reference/Homo_sapiens.GRCh37.67.tar.gz | tar -xz
+wget https://bioinformatics.mdanderson.org/Software/VariantTools/repository/reference/hg19.crr
