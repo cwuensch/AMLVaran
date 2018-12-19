bf08eaa4222524a1bc0f5439d8716418ec34573e
cwuensch <c.wuensch@gmail.com>
2018-12-19 18:53:18 +0100

    Change start script to prepopulate MySQL DB only at first run Add status outputs

--- Docker_start.sh
+++ Docker_start.sh
@@ -1,33 +1,35 @@
 SCRIPT_DIR=$(readlink -f $0)
 SCRIPT_DIR=${SCRIPT_DIR%/*}
 
+echo "Downloading ressources. This may take a few hours..."
+
 cd $SCRIPT_DIR/genomes
 if [ ! -f Homo_sapiens.GRCh37.67.fasta ]; then
+    echo "Get reference genome..."
     sh getReference.sh
 fi
 
 cd $SCRIPT_DIR/genomes/gatk
 if [ ! -f 1000G_phase1.indels.b37.vcf ]; then
+    echo "Get GATK ressources..."
     sh getRessources.sh
 fi
 
 cd $SCRIPT_DIR/genomes/blast
 if [ ! -f nr.05.psq ]; then
+    echo "Get Blast DB for Provean [optional]..."
     sh getBlastDB.sh
 fi
 
-mkdir $SCRIPT_DIR/mysql_data
-wget -qO- https://amlvaran.uni-muenster.de/SQLdump/DB.tar.gz | tar -xz -C $SCRIPT_DIR/mysql_data
+if [ ! -f $SCRIPT_DIR/mysql_data/amlvaran/Variants.ibd ]; then
+    echo "Prepopulating MySQL database..."
+    mkdir $SCRIPT_DIR/mysql_data
+    cd $SCRIPT_DIR/mysql_data
+    wget -qO- https://amlvaran.uni-muenster.de/SQLdump/DB.tar.gz | tar -xz -C $SCRIPT_DIR/mysql_data
+fi
 
+echo "Starting Docker..."
 cd $SCRIPT_DIR
-#docker build -f Dockerfile_db -t amlvaran/db .
-#docker build -f Dockerfile_web -t amlvaran/web .
-#docker build -f Dockerfile -t amlvaran/worker .
-
-#docker run --rm -t amlvaran/db
-#docker run --rm -v /media/watson/projects/christian/amlvaran/samples:/var/samples -v /media/watson/projects/christian/amlvaran/genomes:/var/genomes -t amlvaran/worker
-#docker run --rm -v /media/watson/projects/christian/amlvaran/samples:/var/samples -t amlvaran/web
-
 export HOST_USER_ID=$(id -u)
 export HOST_GROUP_ID=$(id -g)
 docker-compose up
